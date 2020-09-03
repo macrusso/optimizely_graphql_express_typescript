6955084c747c9c6f019abaf621f12b8cea7ee245
macrusso <mac.rusek@pm.me>
2020-09-03 14:51:41 +0100

    added optimizely

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,40 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import optimizelySDK, { Client } from '@optimizely/optimizely-sdk';
 
 dotenv.config()
+const PORT = process.env.PORT;
+const optimizelyClientInstance: Client = optimizelySDK.createInstance({
+  sdkKey: process.env.SDK_KEY
+});
 
 const app: Application = express();
 app.use(cors())
 
-const PORT: string = process.env.PORT || '';
+optimizelyClientInstance.onReady().then(() => {
+  console.log('Optimizely SDK is ready')
+});
 
-app.use('/', (req: Request, res: Response) => {
-  res.status(200).send({
-    data: 'Hello World'
-  });
+app.use('/:userId', (req: Request, res: Response) => {
+  const variation = optimizelyClientInstance.activate('local_test_experiment', req.params.userId);
+  console.log(variation);
+
+  if (variation === 'variation_1') {
+    return res.status(200).send({
+      data: 'variation_1'
+    });
+  } else if (variation === 'variation_2') {
+    return res.status(200).send({
+      data: 'variation_2'
+    });
+  } else {
+    return res.status(200).send({
+      data: 'default'
+    });
+  }
+
+
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
