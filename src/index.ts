948cae300d0f3d8c385fed5165e9f37a7d105b85
macrusso <mac.rusek@pm.me>
2020-09-04 14:44:24 +0100

    added graphql

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
 import optimizelySDK, { Client } from '@optimizely/optimizely-sdk';
 
 dotenv.config()
@@ -12,11 +14,42 @@ const optimizelyClientInstance: Client = optimizelySDK.createInstance({
 const app: Application = express();
 app.use(cors())
 
+const schema = buildSchema(`
+  type Query {
+    getVariation(userId: String!): String
+  }
+`);
+
+interface Args {
+  userId: string
+}
+
+const root = {
+  getVariation: (args: Args) => {
+    const variation = optimizelyClientInstance.activate('local_test_experiment', args.userId);
+    console.log(variation);
+
+    if (variation === 'variation_1') {
+      return 'variation_1'
+    } else if (variation === 'variation_2') {
+      return 'variation_2'
+    } else {
+      return 'default'
+    }
+  },
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
 optimizelyClientInstance.onReady().then(() => {
   console.log('Optimizely SDK is ready')
 });
 
-app.use('/:userId', (req: Request, res: Response) => {
+app.use('/rest/:userId', (req: Request, res: Response) => {
   const variation = optimizelyClientInstance.activate('local_test_experiment', req.params.userId);
   console.log(variation);
 
